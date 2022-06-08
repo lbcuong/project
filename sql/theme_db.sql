8630c2bcd9879ed1be047b360c043343148ee275
Toan1997 <duytoanqng@gmail.com>
2022-06-08 14:00:02 +0700

    Update theme_db.sql

--- sql/theme_db.sql
+++ sql/theme_db.sql
@@ -35,6 +35,45 @@ INSERT INTO `measuring_units` (`id`, `code`, `name`, `comment`, `created_at`, `u
 (9,	'VT00009',	'MARIAH',	NULL,	'2022-06-08 04:19:29',	'2022-06-08 04:19:29'),
 (10,	'VT00010',	'Toan Dao',	'aaaaxccc',	'2022-06-08 06:18:56',	'2022-06-08 06:19:01');
 
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `name_other` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `cluster` varchar(255) DEFAULT NULL,
+  `color` varchar(255) DEFAULT NULL,
+  `size` varchar(255) DEFAULT NULL,
+  `warranty` date DEFAULT NULL,
+  `product_group_id` int(11) DEFAULT NULL,
+  `product_category_id` int(11) DEFAULT NULL,
+  `measuring_unit_id` int(11) DEFAULT NULL,
+  `measuring_unit_conversion_id` int(11) DEFAULT NULL,
+  `producer_id` int(11) DEFAULT NULL,
+  `container_id` int(11) DEFAULT NULL,
+  `barcode` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `limit` varchar(255) DEFAULT NULL,
+  `storage_time` date DEFAULT NULL,
+  `productable_type` varchar(255) DEFAULT NULL,
+  `productable_id` int(11) DEFAULT NULL,
+  `introduction_date` date DEFAULT NULL,
+  `sales_discontinuation_date` date DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_products_measuring_units_id` (`name`),
+  KEY `FK_products_product_categories_id` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `products` (`id`, `code`, `name`, `name_other`, `description`, `cluster`, `color`, `size`, `warranty`, `product_group_id`, `product_category_id`, `measuring_unit_id`, `measuring_unit_conversion_id`, `producer_id`, `container_id`, `barcode`, `image`, `comment`, `limit`, `storage_time`, `productable_type`, `productable_id`, `introduction_date`, `sales_discontinuation_date`, `created_at`, `updated_at`) VALUES
+(1,	'Dmvt00001',	'Khóa nước Ø21',	'Khóa',	'nước',	NULL,	NULL,	'Ø21',	'2022-05-09',	1,	1,	1,	NULL,	2,	NULL,	NULL,	'app-assets/images/inventory/Turkish_Van_Cat.jpeg',	NULL,	NULL,	'2022-05-09',	'App\\Models\\Goods',	1,	NULL,	NULL,	'2022-05-09 06:59:48',	'2022-05-09 07:49:34'),
+(2,	'Dmvt00002',	'Sơn trắng TOA',	'Sơn',	NULL,	NULL,	'Trắng',	NULL,	'2022-05-11',	1,	3,	2,	NULL,	7,	NULL,	'0111234',	NULL,	NULL,	NULL,	'2022-05-09',	'App\\Models\\Goods',	2,	NULL,	NULL,	'2022-05-09 07:14:12',	'2022-05-09 07:14:12'),
+(3,	'Dmvt00003',	'Sơn BENDE',	'Sơn',	NULL,	NULL,	NULL,	NULL,	'2022-05-10',	1,	3,	2,	NULL,	1,	NULL,	NULL,	NULL,	NULL,	NULL,	'2022-05-18',	'App\\Models\\Goods',	3,	NULL,	NULL,	'2022-05-09 07:19:16',	'2022-05-09 07:19:16'),
+(4,	'Dmvt00003',	'Sơn BENDE',	'Sơn',	NULL,	NULL,	NULL,	NULL,	'2022-05-10',	1,	3,	2,	NULL,	1,	NULL,	NULL,	NULL,	NULL,	NULL,	'2022-05-18',	'App\\Models\\Goods',	4,	NULL,	NULL,	'2022-05-09 07:19:16',	'2022-05-09 07:19:16');
+
 DROP TABLE IF EXISTS `reorder_guidelines`;
 CREATE TABLE `reorder_guidelines` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -80,4 +119,4 @@ INSERT INTO `reorder_items` (`id`, `reorder_quantity`, `reorder_guideline_id`, `
 (10,	10,	NULL,	1,	'10xxx',	'App\\Models\\ReorderGuideline',	4,	'2022-06-08 06:18:18',	'2022-06-08 06:18:23'),
 (11,	10,	5,	1,	'10',	'App\\Models\\ReorderGuideline',	5,	'2022-06-08 06:18:40',	'2022-06-08 06:18:40');
 
--- 2022-06-08 06:37:56
+-- 2022-06-08 06:59:28
